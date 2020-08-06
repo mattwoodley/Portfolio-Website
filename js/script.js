f6204dff0683bcabd261e58f4241abb0a8a8b61b
Matthew Woodley <matt@mattwoodley.net>
2020-08-06 10:13:58 +0100

    added folder structure + added github linkedin hrefs

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ const currentTheme = localStorage.getItem('theme');
 
 setTheme = (theme) => {
     if (theme === 'light') {
-        document.getElementById("theme-style").href = 'default.css'
+        document.getElementById("theme-style").href = '../css/default.css'
     } else if (theme === 'blue') {
-        document.getElementById("theme-style").href = 'theme-blue.css'
+        document.getElementById("theme-style").href = '../css/theme/theme-blue.css'
     } else if (theme === 'green') {
-        document.getElementById("theme-style").href = 'theme-green.css'
+        document.getElementById("theme-style").href = '../css/theme/theme-green.css'
     } else if (theme === 'purple') {
-        document.getElementById("theme-style").href = 'theme-purple.css'
+        document.getElementById("theme-style").href = '../css/theme/theme-purple.css'
     }
     
     localStorage.setItem('theme', theme);
