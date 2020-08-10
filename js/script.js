a53a543d5c58c285181cff9c93c2f9e84af977e0
Matthew Woodley <matt@mattwoodley.net>
2020-08-10 17:14:33 +0100

    smooth-scroll, contact anchor, removed green-theme, theme purple->pink, if checks for themeConfirmation and setTheme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const themeDots = document.getElementsByClassName('theme-dot');
+const leftColumn = document.querySelector('.left-column');
 const currentTheme = localStorage.getItem('theme');
-
 
 setTheme = (theme) => {
     if (theme === 'light') {
@@ -9,8 +9,8 @@ setTheme = (theme) => {
         document.getElementById("theme-style").href = '../css/theme/theme-blue.css'
     } else if (theme === 'green') {
         document.getElementById("theme-style").href = '../css/theme/theme-green.css'
-    } else if (theme === 'purple') {
-        document.getElementById("theme-style").href = '../css/theme/theme-purple.css'
+    } else if (theme === 'pink') {
+        document.getElementById("theme-style").href = '../css/theme/theme-pink.css'
     }
     
     localStorage.setItem('theme', theme);
@@ -25,6 +25,27 @@ if (currentTheme === null) {
 for (let i=0; themeDots.length > i; i++){
     themeDots[i].addEventListener('click', () => {
         let theme = themeDots[i].dataset.theme;
-        setTheme(theme);
+        const localStorageTheme = localStorage.getItem('theme');
+        
+        if (themeDots[i].dataset.theme === localStorageTheme) {
+            return;
+        } else {
+            setTheme(theme);
+            themeConfirmation();
+        }
     });
+}
+
+themeConfirmation = () => {
+    if (leftColumn.querySelectorAll("p").length === 1) {
+        // if themeConfirmation already exists then don't add another paragraph as it leads to textContent repeating.
+        return;
+    } else {
+        const themeConfirmation = document.createElement('p');
+        themeConfirmation.textContent = "*Theme settings will be saved for your next visit";
+        themeConfirmation.setAttribute("id", "settings-note");
+        leftColumn.appendChild(themeConfirmation);
+        window.setTimeout(() => themeConfirmation.remove(), 4000);
+    }
+
 }
